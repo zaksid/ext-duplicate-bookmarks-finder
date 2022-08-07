08e600c13252bf664cca9df7026c30c08795258c
Oleksandr Mahdybor <o.mahdybor@astoundcommerce.com>
2022-08-07 10:51:29 +0300

    Refactor (move classes to modules). Added loader

---
+++ js/Bookmarks.js
@@ -0,0 +1,70 @@
+export default class Bookmarks {
+    constructor(bookmarksTree) {
+        this.processedBookmarks = [];
+        this.#prepareBookmarks(bookmarksTree);
+    }
+
+    #prepareBookmarks(bookmarksInput, path = '') {
+        // Make a copy of an array to work with
+        const bookmarks = bookmarksInput.slice();
+
+        for (let i = 0; i < bookmarks.length; i++) {
+            const bookmark = bookmarks[i];
+
+            if (bookmark.url) {
+                if (path) {
+                    if (!('myPath' in bookmark)) {
+                        bookmark.myPath = '';
+                    }
+                    bookmark.myPath += path + '/';
+                }
+                this.processedBookmarks.push(bookmark);
+            }
+
+            if (bookmark.children) {
+                this.#prepareBookmarks(bookmark.children, `${path ? path + '/' : ''}${bookmark.title}`);
+            }
+        }
+    }
+
+    getBookmarks() {
+        return this.processedBookmarks;
+    }
+
+    getDuplicates(ignoredUrls) {
+        const array = this.processedBookmarks.slice();
+        const matches = {};
+        const httpsRegex = /http(s)?/;
+        const ignoredUrlsRegex = new RegExp(ignoredUrls);
+
+        for (let i = 0; i < array.length; i++) {
+            let currentElem = array[i];
+            let wasMatched = false;
+
+            if (ignoredUrls && ignoredUrlsRegex.test(currentElem.url)) {
+                continue;
+            }
+
+            for (let j = i + 1; j < array.length; j++) {
+                let url1 = currentElem.url.replace(httpsRegex);
+                let url2 = array[j].url.replace(httpsRegex);
+
+                if (url1 === url2) {
+                    if (!(i in matches)) {
+                        matches[i] = [];
+                    }
+                    matches[i].push(array[j]);
+                    array.splice(j, 1);
+                    wasMatched = true;
+                    j--; // adjust due to deleted element
+                }
+            }
+
+            if (wasMatched) {
+                matches[i].unshift(currentElem);
+            }
+        }
+
+        return Object.values(matches);
+    }
+}
---
+++ js/Browser.js
@@ -0,0 +1,31 @@
+export default class Browser {
+    constructor(browser) {
+        this.browser = browser;
+        this.i18n = browser.i18n;
+    }
+
+    getBookmarksTree() {
+        // Will work in Chrome with manifest v3
+        // return this.browser.bookmarks.getTree();
+
+        return new Promise((resolve) => {
+            this.browser.bookmarks.getTree((results) => {
+                resolve(results)
+            });
+        });
+    }
+
+    getExtensionVersion() {
+        return this.browser.runtime.getManifest().version;
+    }
+
+    removeBookmarks(ids = []) {
+        ids.forEach(async (id) => {
+            try {
+                await this.browser.bookmarks.remove(id);
+            } catch (e) {
+                console.error(`Cannot delete bookmark with id ${id}: ${e}`);
+            }
+        });
+    }
+}
