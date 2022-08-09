c51869f38a0df927169fdf47b7880c35d9bd118c
Oleksandr Mahdybor <o.mahdybor@astoundcommerce.com>
2022-08-09 13:35:36 +0300

    🛠 Updated project structure and build process

--- set-version.sh
+++ set-version.sh
@@ -18,6 +18,6 @@ then
     exit 1
 fi
 
-sed -i '' -r -E "s/(\"version\": \")([0-9]+\.[0-9]+\.?[0-9]?)(\")/\1$version\3/g" manifest.json
-sed -i '' -r -E "s/(\"version\": \")([0-9]+\.[0-9]+\.?[0-9]?)(\")/\1$version\3/g" manifest_v2.json
+sed -i '' -r -E "s/(\"version\": \")([0-9]+\.[0-9]+\.?[0-9]?)(\")/\1$version\3/g" platform/chromium/manifest.json
+sed -i '' -r -E "s/(\"version\": \")([0-9]+\.[0-9]+\.?[0-9]?)(\")/\1$version\3/g" platform/firefox/manifest.json
 sed -i '' -r -E "s/(version-)([0-9]+\.[0-9]+\.?[0-9]?)(\.*)/\1$version\3/g" README.md
