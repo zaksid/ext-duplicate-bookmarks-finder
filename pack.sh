c934da737c10a3b46018219835fe5e467acec629
Oleksandr Mahdybor <o.mahdybor@astoundcommerce.com>
2022-08-12 14:16:24 +0300

    ⭕️ Changes to support building for Opera

--- pack.sh
+++ pack.sh
@@ -25,7 +25,11 @@ while getopts ":p:m:h" o; do
 
             if [[ "$arg" == "firefox" ]]; then
                 platform="firefox"
+            elif [[ "$arg" == "opera" ]]; then
+                platform="opera"
             fi
+
+            echo -e "> Platform: ${platform}"
             ;;
         m)
             arg=${OPTARG}
@@ -33,6 +37,8 @@ while getopts ":p:m:h" o; do
             if [[ "$arg" == "dev" ]]; then
                 mode="development"
             fi
+
+            echo -e "> Mode: ${mode}"
             ;;
         h)
             is_help=true
@@ -54,14 +60,30 @@ fi
 mkdir build
 cp -r ./src/* build/
 rm -f build/manifest*.*
-cp ./platform/${platform}/manifest.json build
 
-if [[ "$mode" == "development" ]]; then
+if [[ "$platform" == "opera" ]]; then
+    cp ./platform/chromium/manifest.json build
+else
+    cp ./platform/${platform}/manifest.json build
+fi
+
+if [[ "$platform" == "opera" ]]; then
+    echo -e "> Renaming mustashe templates for Opera..."
+    find build/templates/ -name '*.mustache' -exec sh -c 'mv "$0" "${0%.mustache}.mustache.html"' {} \;
+    sed -i '' -r -E "s/\.mustache/\.mustache\.html/g" build/js/popup.js
+    echo -e "> ...done"
+fi
+
+#if [[ "$mode" == "development" ]]; then
     cd build
     zip -r extension-${platform}.zip *
     mv extension-${platform}.zip ../
     cd ../
-fi
+
+    if [[ "$platform" == "opera" ]]; then
+        mv extension-${platform}.zip extension-${platform}.crx
+    fi
+#fi
 
 if [[ "$mode" == "publishing" ]]; then
     rm -rf build
